edb7872699eeadb3fc9e2cc4a40beb00fbf429e0
rohitkatore <142085054+rohitkatore@users.noreply.github.com>
2024-12-28 21:44:15 +0530

    Update Verify.jsx

--- Frontend/src/pages/Verify/Verify.jsx
+++ Frontend/src/pages/Verify/Verify.jsx
@@ -15,7 +15,7 @@ function Verify() {
     const verifyPayment = async()=>{
         const response = await axios.post(url+"/api/order/verify",{success,orderId}) ;
         if(response.data.success){
-            navigate("https://tomato-ochre.vercel.app/myorders") ;
+            navigate("https://tomato26.vercel.app/myorders") ;
         }else{
             navigate("/") ;
         }
