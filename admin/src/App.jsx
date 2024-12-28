64002ecf786cc080e53185ccdce868eb19be92a9
rohitkatore <142085054+rohitkatore@users.noreply.github.com>
2024-12-28 20:02:41 +0530

    Update App.jsx
    url change

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const url = "http://localhost:4000" ;
+  const url = "https://tomato-7jmb.onrender.com" ;
   return (
     <div>
       <ToastContainer/>
