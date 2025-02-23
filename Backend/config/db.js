c169d0294bb3e5d142df52f22a66586bd4aad472
rohitkatore <142085054+rohitkatore@users.noreply.github.com>
2025-02-23 18:00:00 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-let url = "mongodb+srv://rohitkatore645:N9gl0Q8UwiE37ZXf@foodzone.xw9ya.mongodb.net/?retryWrites=true&w=majority&appName=foodzone" ;
+let url = process.env.MONGOOSE_URL ;
 
 export const connectDB = async ()=>{
     await mongoose.connect(url).then(()=>
